4ac79901dcd1d2f63b3e27eb188a3e3eb7706676
fulviocanducci <fulviocanducci@hotmail.com>
2017-06-21 16:16:44 -0300

    News 1

---
+++ MongoDBCRUD/Entities/News.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace MongoDBCRUD.Entities
+{    
+    public class News
+    {
+        public News()
+        {
+        }
+
+        [BsonId()]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("title")]
+        [BsonRequired()]
+        public string Title { get; set; }
+
+        [BsonElement("body")]
+        [BsonRequired()]
+        public string Body { get; set; }
+
+        [BsonElement("created")]
+        [BsonRequired()]
+        public DateTime Created { get; set; }
+
+        [BsonElement("active")]
+        [BsonRequired()]
+        public bool Active { get; set; }
+    }
+}
--- MongoDBCRUD/Program.cs
+++ MongoDBCRUD/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDBCRUD.Entities;
+using System;
 
 namespace MongoDBCRUD
 {
@@ -6,7 +9,31 @@ namespace MongoDBCRUD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            IMongoClient client = new MongoClient("mongodb://localhost");
+            IMongoDatabase database = client.GetDatabase("example");            
+            IMongoCollection<News> colNews = database.GetCollection<News>("news");
+
+            //News doc = new News();
+            //doc.Title = "Titulo 2";
+            //doc.Body = "Texto 2";
+            //doc.Created = DateTime.Now;
+            //doc.Active = true;
+
+
+
+
+            //colNews.InsertOne(doc);
+
+            UpdateDefinition<News> updateNews = Builders<News>.Update.Set(a => a.Active, false);
+            var result = colNews.UpdateMany(up => up.Title.Contains("t"), updateNews);
+
+            News c = colNews                
+                .Find(x => x.Id.Equals(ObjectId.Parse("594ac1257127cd2a447c5d25")))
+                .FirstOrDefault();
+
+
+            Console.WriteLine("Item inserido com êxito");
         }
     }
 }
